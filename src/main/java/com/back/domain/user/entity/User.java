adb964739d1ff765f6a373a5b886cebddc2f6474
meohin <meohin.meo@gmail.com>
2025-09-17 15:55:02 +0900

    fix: User 엔티티에서 불필요한 필드 삭제

--- src/main/java/com/back/domain/user/entity/User.java
+++ src/main/java/com/back/domain/user/entity/User.java
@@ -1,12 +1,19 @@
 package com.back.domain.user.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "users") // DB 테이블 이름: User 대신 users 권장 (예약어 충돌 방지)
+@Table(
+        name = "users", // DB 테이블 이름: User 대신 users 권장 (예약어 충돌 방지)
+        indexes = {
+                @Index(name = "ux_users_email", columnList = "email", unique = true)
+        }
+)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,13 +25,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Email
+    @Column(nullable = false, unique = true)
     private String email;   // 유저 소셜 이메일 (OAuth2 로그인 시 저장)
 
+    @NotBlank
+    @Column(nullable = false, unique = true)
     private String nickname;   // 유저 닉네임
 
     private String profileImgUrl;   // 프로필 이미지 URL
-
-    private String apiKey;   // 리프레시 토큰 (쿠키 인증과 연동)
 
     private Double abvDegree;   // 온도(회원 등급)
 
