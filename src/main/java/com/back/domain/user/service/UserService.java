198db376cfefa3f02e578362d6fe0a8371489eee
meohin <meohin.meo@gmail.com>
2025-09-18 12:10:03 +0900

    feat: UserService 추가

---
+++ src/main/java/com/back/domain/user/service/UserService.java
@@ -0,0 +1,20 @@
+package com.back.domain.user.service;
+
+import com.back.domain.user.entity.User;
+import com.back.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("User not found. id=" + id));
+    }
+}
