5638bca898262a7114c495ddc4248198d3485f25
meohin <meohin.meo@gmail.com>
2025-09-18 16:24:18 +0900

    feat: 사용자 프로필 수정 API 추가

--- src/main/java/com/back/domain/profile/controller/ProfileController.java
+++ src/main/java/com/back/domain/profile/controller/ProfileController.java
@@ -1,14 +1,14 @@
 package com.back.domain.profile.controller;
 
 import com.back.domain.profile.dto.ProfileResponseDto;
+import com.back.domain.profile.dto.ProfileUpdateRequestDto;
 import com.back.domain.profile.service.ProfileService;
 import com.back.domain.user.service.UserService;
 import com.back.global.rsData.RsData;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/me/profile")
@@ -22,5 +22,11 @@ public class ProfileController {
     public RsData<ProfileResponseDto> getProfile(@AuthenticationPrincipal(expression = "id") Long userId) {
         ProfileResponseDto body = profileService.getProfile(userId);
         return RsData.successOf(body); // code=200, message="success"
+    }
+
+    @PutMapping
+    public RsData<ProfileResponseDto> updateProfile(@AuthenticationPrincipal(expression = "id") Long userId, @Valid @RequestBody ProfileUpdateRequestDto profileUpdateRequestDto) {
+        ProfileResponseDto body = profileService.updateProfile(userId, profileUpdateRequestDto);
+        return RsData.successOf(body); // code=200
     }
 }
