6027b162c155c2dce025964cb6d6b539cc4b9bef
meohin <meohin.meo@gmail.com>
2025-09-19 14:49:26 +0900

    fix typo

--- src/main/java/com/back/domain/mybar/controller/MyBarController.java
+++ src/main/java/com/back/domain/mybar/controller/MyBarController.java
@@ -7,13 +7,13 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Repository;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Repository
+@RestController
 @RequestMapping("/me/bar")
 @RequiredArgsConstructor
 @Validated
