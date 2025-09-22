f637f7b84b27131be4a3b02e0a01de118d786c3b
SeokGeunHo <kimc6555@gmail.com>
2025-09-22 15:40:23 +0900

    feat: 게시글 수정 구현

--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -2,7 +2,8 @@ package com.back.domain.post.post.service;
 
 import com.back.domain.post.category.entity.Category;
 import com.back.domain.post.category.repository.CategoryRepository;
-import com.back.domain.post.post.dto.request.PostRequestDto;
+import com.back.domain.post.post.dto.request.PostCreateRequestDto;
+import com.back.domain.post.post.dto.request.PostUpdateRequestDto;
 import com.back.domain.post.post.dto.response.PostResponseDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.entity.Tag;
@@ -28,34 +29,23 @@ public class PostService {
 
   // 게시글 작성 로직
   @Transactional
-  public PostResponseDto createPost(PostRequestDto postRequestDto) {
+  public PostResponseDto createPost(PostCreateRequestDto reqBody) {
     User user = rq.getActor(); // 현재 로그인한 사용자의 정보 가져오기
 
-    Category category = categoryRepository.findById(postRequestDto.categoryId())
-        .orElseThrow(() -> new IllegalArgumentException("해당 카테고리를 찾을 수 없습니다. ID: " + postRequestDto.categoryId()));
+    Category category = categoryRepository.findById(reqBody.categoryId())
+        .orElseThrow(() -> new IllegalArgumentException("해당 카테고리를 찾을 수 없습니다. ID: " + reqBody.categoryId()));
 
     Post post = Post.builder()
         .category(category)
         .user(user)
-        .title(postRequestDto.title())
-        .content(postRequestDto.content())
-        .imageUrl(postRequestDto.imageUrl())
+        .title(reqBody.title())
+        .content(reqBody.content())
+        .imageUrl(reqBody.imageUrl())
         .build();
 
-    List<String> tagNames = postRequestDto.tags();
+    List<String> tagNames = reqBody.tags();
     if (tagNames != null && !tagNames.isEmpty()) {
-      for (String tagName : tagNames) {
-        // 태그 이름으로 Tag 엔티티를 조회하거나, 없으면 새로 생성하여 저장
-        Tag tag = tagRepository.findByName(tagName)
-            .orElseGet(() -> tagRepository.save(
-                    Tag.builder()
-                        .name(tagName)
-                        .build()
-                )
-            );
-
-        post.addTag(tag);
-      }
+      addTag(tagNames, post);
     }
 
     return new PostResponseDto(postRepository.save(post));
@@ -78,5 +68,56 @@ public class PostService {
         postRepository.findById(postId)
             .orElseThrow(() -> new NoSuchElementException("해당 게시글을 찾을 수 없습니다. ID: " + postId))
     );
+  }
+
+  // 게시글 수정 로직
+  @Transactional
+  public PostResponseDto updatePost(Long postId, PostUpdateRequestDto reqBody) {
+    Post post = postRepository.findById(postId)
+        .orElseThrow(() -> new NoSuchElementException("해당 게시글을 찾을 수 없습니다. ID: " + postId));
+
+    if (reqBody.categoryId() != null) {
+      Category category = categoryRepository.findById(reqBody.categoryId())
+          .orElseThrow(() -> new IllegalArgumentException(
+              "해당 카테고리를 찾을 수 없습니다. ID: " + reqBody.categoryId()
+              )
+          );
+      post.updateCategory(category);
+    }
+    if (reqBody.status() != null) {
+      post.updateStatus(reqBody.status());
+    }
+    if (reqBody.title() != null && !reqBody.title().isBlank()){
+      post.updateTitle(reqBody.title());
+    }
+    if (reqBody.content() != null && !reqBody.content().isBlank()) {
+      post.updateContent(reqBody.content());
+    }
+    if (reqBody.imageUrl() != null && !reqBody.imageUrl().isBlank()) {
+      post.updateImage(reqBody.imageUrl());
+    }
+    if (reqBody.tags() != null) {
+      // 기존 태그들 삭제
+      post.clearTags();
+
+      // 새로운 태그들 추가
+      addTag(reqBody.tags(), post);
+    }
+
+    return new PostResponseDto(post);
+  }
+
+  // 태그 추가 메서드
+  private void addTag(List<String> tagNames, Post post) {
+    for (String tagName : tagNames) {
+      Tag tag = tagRepository.findByName(tagName)
+          .orElseGet(() -> tagRepository.save(
+              Tag.builder()
+                  .name(tagName)
+                  .build()
+              )
+          );
+      post.addTag(tag);
+    }
   }
 }
