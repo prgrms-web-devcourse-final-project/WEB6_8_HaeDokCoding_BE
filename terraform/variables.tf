b1dd5b49b99b361737b36120bb6ba07ed58c30c2
seungwookc97 <sochoi0832@gmail.com>
2025-09-23 18:09:23 +0900

    fix : DATABSE_URL 환경변수 수정

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "instance_type" {
   type = string                     # The type of the variable, in this case a string
-  default = "t2.micro"                 # Default value for the variable
+  default = "t3.micro"                 # Default value for the variable
   description = "The type of EC2 instance" # Description of what this variable represents
 }
 
