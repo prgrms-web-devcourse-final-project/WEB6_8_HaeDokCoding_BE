dd32514fe43e911619e795582f128a72ac2fe42c
seungwookc97 <sochoi0832@gmail.com>
2025-09-23 12:17:37 +0900

    feat : terraform 설정

---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "instance_type" {
+  type = string                     # The type of the variable, in this case a string
+  default = "t2.micro"                 # Default value for the variable
+  description = "The type of EC2 instance" # Description of what this variable represents
+}
+
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "team2"
+}
+
+variable "app_1_domain" {
+  description = "app_1 domain"
+  default     = "api.p-14044-1.oa.gg"
+}
