fba58cbc1343ecd957abb9422fe5c5d23d436e74
LeeKW <lkw9241@gmail.com>
2025-09-24 11:44:51 +0900

    feat : CocktailCommentResponseDto

--- src/main/java/com/back/domain/cocktail/comment/dto/CocktailCommentResponseDto.java
+++ src/main/java/com/back/domain/cocktail/comment/dto/CocktailCommentResponseDto.java
@@ -1,4 +1,28 @@
 package com.back.domain.cocktail.comment.dto;
 
-public class CocktailCommentResponseDto {
+import com.back.domain.cocktail.comment.entity.CocktailComment;
+import com.back.domain.post.comment.enums.CommentStatus;
+
+import java.time.LocalDateTime;
+
+public record CocktailCommentResponseDto(
+        Long commentId,
+        Long postId,
+        String userNickName,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt,
+        CommentStatus status,
+        String content
+) {
+    public CocktailCommentResponseDto(CocktailComment cocktailcomment) {
+        this(
+                cocktailcomment.getId(),
+                cocktailcomment.getCocktail().getCocktailId(),
+                cocktailcomment.getUser().getNickname(),
+                cocktailcomment.getCreatedAt(),
+                cocktailcomment.getUpdatedAt(),
+                cocktailcomment.getStatus(),
+                cocktailcomment.getContent()
+        );
+    }
 }
--- src/main/java/com/back/domain/cocktail/entity/Cocktail.java
+++ src/main/java/com/back/domain/cocktail/entity/Cocktail.java
@@ -5,7 +5,11 @@ import com.back.domain.cocktail.enums.AlcoholStrength;
 import com.back.domain.cocktail.enums.CocktailType;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import java.time.LocalDateTime;
+
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 
@@ -40,7 +44,9 @@ public class Cocktail {
 
     private String cocktailImgUrl;
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
