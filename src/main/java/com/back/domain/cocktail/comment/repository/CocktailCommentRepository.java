40e317adc44dda345cf469d5d530734ceace008e
LeeKW <lkw9241@gmail.com>
2025-09-24 17:52:00 +0900

    fix : bug

--- src/main/java/com/back/domain/cocktail/comment/repository/CocktailCommentRepository.java
+++ src/main/java/com/back/domain/cocktail/comment/repository/CocktailCommentRepository.java
@@ -2,9 +2,11 @@ package com.back.domain.cocktail.comment.repository;
 
 import com.back.domain.cocktail.comment.entity.CocktailComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CocktailCommentRepository extends JpaRepository<CocktailComment, Long> {
 
     List<CocktailComment> findTop10ByCocktailIdOrderByIdDesc(Long cocktailId);
