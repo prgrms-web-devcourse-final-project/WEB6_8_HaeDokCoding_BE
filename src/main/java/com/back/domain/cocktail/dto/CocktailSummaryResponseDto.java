2f09536b97014ee6b523d2cc2b794f7b0e1b1da8
LeeKW <lkw9241@gmail.com>
2025-09-26 08:58:25 +0900

    fix : dto, service

--- src/main/java/com/back/domain/cocktail/dto/CocktailSummaryResponseDto.java
+++ src/main/java/com/back/domain/cocktail/dto/CocktailSummaryResponseDto.java
@@ -1,5 +1,6 @@
 package com.back.domain.cocktail.dto;
 
+import com.back.domain.cocktail.enums.AlcoholStrength;
 import lombok.Getter;
 
 @Getter
@@ -7,10 +8,12 @@ public class CocktailSummaryResponseDto {
     private Long cocktailId;
     private String cocktailName;
     private String cocktailImgUrl;
+    private AlcoholStrength alcoholStrength;
 
-    public CocktailSummaryResponseDto(Long id, String name, String imageUrl) {
+    public CocktailSummaryResponseDto(Long id, String name, String imageUrl, AlcoholStrength alcoholStrength) {
         this.cocktailId = id;
         this.cocktailName = name;
         this.cocktailImgUrl = imageUrl;
+        this.alcoholStrength = alcoholStrength;
     }
 }
