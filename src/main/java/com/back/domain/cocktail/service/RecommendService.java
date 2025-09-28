927c767985b0593c6e837b860fde614d1ac9ea92
LeeKW <lkw9241@gmail.com>
2025-09-28 15:03:06 +0900

    fix : recommend logix

--- src/main/java/com/back/domain/cocktail/service/RecommendService.java
+++ src/main/java/com/back/domain/cocktail/service/RecommendService.java
@@ -6,10 +6,7 @@ import com.back.domain.cocktail.repository.CocktailRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -21,24 +18,20 @@ public class RecommendService {
         Cocktail current = cocktailRepository.findById(cocktailId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 칵테일입니다."));
 
-        // 3가지 조건으로 유사 칵테일 조회
-        List<Cocktail> byAlcoholStrength = cocktailRepository.findByAlcoholStrengthAndIdNot(current.getAlcoholStrength(), current.getId());
-        List<Cocktail> byCocktailType = cocktailRepository.findByCocktailTypeAndIdNot(current.getCocktailType(), current.getId());
-        List<Cocktail> byAlcoholBase = cocktailRepository.findByAlcoholBaseTypeAndIdNot(current.getAlcoholBaseType(), current.getId());
+       // 알콜 강도와 베이스 타이이 같은 칵테일만 조회
+        List<Cocktail> related = cocktailRepository
+                .findByAlcoholStrengthAndAlcoholBaseTypeAndIdNot(
+                        current.getAlcoholStrength(),
+                        current.getAlcoholBaseType(),
+                        current.getId()
+                );
 
-        // 합치고 중복 제거
-        Set<Cocktail> combined = new LinkedHashSet<>();
-        combined.addAll(byAlcoholStrength);
-        combined.addAll(byCocktailType);
-        combined.addAll(byAlcoholBase);
-
-        List<Cocktail> combinedList = new ArrayList<>(combined);
-        if (combinedList.size() > maxSize) {
-            combinedList = combinedList.subList(0, maxSize);
+        if (related.size() > maxSize) {
+            related = related.subList(0, maxSize);
         }
 
         // DTO로 변환
-        return combinedList.stream()
+        return related.stream()
                 .map(c -> new CocktailRecommendResponseDto(
                         c.getId(),
                         c.getCocktailNameKo(),
